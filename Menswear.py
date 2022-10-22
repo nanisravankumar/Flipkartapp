8dc21b384a37fe886731ccc6ad9e6a14f29ab8e9
nanisravankumar <pulle.sravankumar@gmail.com>
2022-10-22 20:52:13 +0530

    soujanya developer

--- Menswear.py
+++ Menswear.py
@@ -9,35 +9,7 @@ SPEED_OF_LIGHT = 3e8  # unit of c : m * s^-1
 def casimir_force(force: float, area: float, distance: float) -> dict[str, float]:
 
     """
-    Input Parameters
-    ----------------
-    force -> Casimir Force : magnitude in Newtons
-    area -> Surface area of each plate : magnitude in square meters
-    distance -> Distance between two plates : distance in Meters
-    Returns
-    -------
-    result : dict name, value pair of the parameter having Zero as it's value
-    Returns the value of one of the parameters specified as 0, provided the values of
-    other parameters are given.
-    >>> casimir_force(force = 0, area = 4, distance = 0.03)
-    {'force': 6.4248189174864216e-21}
-    >>> casimir_force(force = 2635e-13, area = 0.0023, distance = 0)
-    {'distance': 1.0323056015031114e-05}
-    >>> casimir_force(force = 2737e-21, area = 0, distance = 0.0023746)
-    {'area': 0.06688838837354052}
-    >>> casimir_force(force = 3457e-12, area = 0, distance = 0)
-    Traceback (most recent call last):
-        ...
-    ValueError: One and only one argument must be 0
-    >>> casimir_force(force = 3457e-12, area = 0, distance = -0.00344)
-    Traceback (most recent call last):
-        ...
-    ValueError: Distance can not be negative
-    >>> casimir_force(force = -912e-12, area = 0, distance = 0.09374)
-    Traceback (most recent call last):
-        ...
-    ValueError: Magnitude of force can not be negative
-    """
+This is soujanya developer    
 
     if (force, area, distance).count(0) != 1:
         raise ValueError("One and only one argument must be 0")
